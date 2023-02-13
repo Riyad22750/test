27aea412951850f49f1c033f22f3786499e4d441
Riyad22750 <112902985+Riyad22750@users.noreply.github.com>
2023-02-13 16:00:10 +0600

    Create Test.sh

---
+++ Test.sh
@@ -0,0 +1,23 @@
+>
+
+<head>
+
+<title> riyad chowdhury </title>
+
+</head>
+
+<body bgcolor="black">
+
+<font size="10px" color="white">
+
+<h1><p align="center" ><i> Hacked by Riyad Chowdhury </i></p> </h1>
+
+<u><b><i>my name is heron chowdhury</i></b></u><br>sql ingaction by first time website<br>than im a interest website hacking
+
+</font>
+
+<img src=""/>
+
+</body>
+
+</html>
